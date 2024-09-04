b0f4f1552112d970e1a0a757e15710328f8a0327
Norbert0215 <norbert.szabo.2006@gmail.com>
2024-09-04 09:19:49 +0200

    objektumos makeBoxes

--- js/app.js
+++ js/app.js
@@ -1,17 +1,52 @@
 // alert("asdfa")
 
-const makeBoxes = () => {
+//Számokat tartalmazó négyzetek létrehozás
+/* const makeBoxes = () => {
     let boxList = [];
     for (let i = 1; i < 16; i++) {
         boxList.push(`<div id="box-${i}" class="box">${i}</div>`);
     }
     return boxList;
-};
+}; */
 
+const makeBoxes = () =>{
+    const data = [
+        {number: 1},
+        {number: 2},
+        {number: 3},
+        {number: 4},
+        {number: 5},
+        {number: 6},
+        {number: 7},
+        {number: 8},
+        {number: 9},
+        {number: 10},
+        {number: 11},
+        {number: 12},
+        {number: 13},
+        {number: 14},
+        {number: 15}
+    ];
+    const content = data.map()
+}
+//A négyzetek megjelenítése a HTML-ben
 const renderBoxes = () => {
     const boxesContainer = document.querySelector(".boxes");
     boxesContainer.innerHTML = "";
     boxesContainer.innerHTML = makeBoxes().join('');
+    //const boxes = document.querySelectorAll(".box")
+    //console.log("boxlista1: ",boxes);
 };
+//Az oldal betöltésekor inicializálódik a weboldal
+//document.addEventListener("DOMContentLoaded", renderBoxes);
 
-document.addEventListener("DOMContentLoaded", renderBoxes);
+//ezek a kódsorok hamarabb funak le mint a callback részek ezért itt még nem létezik a renderelt tartalom
+//const boxes = document.querySelectorAll(".box")
+//console.log("boxlista1: ",boxes);
+
+document.addEventListener("DOMContentLoaded", () =>{
+    renderBoxes()
+});
+
+
+
